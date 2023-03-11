d9d4becd582d78d239a0f53dc1c4bf07442aa75e
VagAnts <98564398+VagAnts@users.noreply.github.com>
2023-03-11 22:28:49 +0000

    0.8.15

---
+++ gpt.go
@@ -0,0 +1,42 @@
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/hanyuancheung/gpt-go"
+)
+
+func queryGTP(ctx context.Context, txtChan chan string, messages []gpt.ChatCompletionRequestMessage) {
+	fmt.Println("Query messages:")
+	showAsJson(messages)
+	client := gpt.NewClient(
+		getOpenAIkey(),
+		gpt.WithBaseURL(getOpenAIBaseUrl()),
+		gpt.WithTimeout(600*time.Second),
+	)
+
+	err := client.ChatCompletionStream(ctx, &gpt.ChatCompletionRequest{
+		Model:    g_userSetting.model,
+		Messages: messages,
+	}, func(response *gpt.ChatCompletionStreamResponse) {
+		//fmt.Println(response.Choices)
+		//fmt.Printf("%+v\n", response)
+		if len(response.Choices) > 0 {
+			if response.Choices[0].Delta.Content != "" {
+				txtChan <- response.Choices[0].Delta.Content
+			}
+			if response.Choices[0].FinishReason == "stop" {
+				close(txtChan)
+			}
+		}
+	})
+	if err != nil {
+		fmt.Printf("ChatCompletionStream error: %v\n", err)
+		txtChan <- fmt.Sprintf("ChatCompletionStream error: %v\n", err)
+		close(txtChan)
+		return
+	}
+}
